a80a4e81d1eaf1ccfd9db4389de4475423c42d61
Mohammad Rayyan Adhoni <166967645+MoRayyan107@users.noreply.github.com>
2025-08-30 15:04:08 +0400

    Create 1480. Running sum of 1d array.java

---
+++ 1480. Running sum of 1d array.java
@@ -0,0 +1,9 @@
+// Time complexity -> O(n), Space complexity -> O(1) 
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++){
+            nums[i] += nums[i-1];
+        }
+        return nums;
+    }
+}
