2741b7d8a9bd6310c5c94d065a474dccc0c70f1c
Mohammad Rayyan Adhoni <166967645+MoRayyan107@users.noreply.github.com>
2025-08-30 15:31:17 +0400

    Create 1672. Richest Customer Wealth.java

---
+++ 1672. Richest Customer Wealth.java
@@ -0,0 +1,15 @@
+// Time complexity -> (O(n*m), Space complexity -> O(n)
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for(int i = 0; i < accounts.length; i++){
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++){
+                sum += accounts[i][j];
+            }
+            if (sum > max) max = sum;
+        }
+        return max;
+    }
+}
